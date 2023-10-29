d61b0502a4d241058a9d038f061ea63bbf2265c0
occiandiaali <ocean.diaali@gmail.com>
2023-10-29 12:39:36 +0100

    Initial commit

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,23 +1,155 @@
 <template>
   <ion-page>
-    <ion-header>
+    <!-- <ion-header>
       <ion-toolbar>
         <ion-title>Tab 1</ion-title>
       </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
+    </ion-header> -->
+    <ion-content :fullscreen="true" class="ion-padding">
+      <!-- <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
-      </ion-header>
+      </ion-header> -->
+      <ion-chip>
+    <ion-avatar>
+      <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+    </ion-avatar>
+    <ion-label>@admin3000</ion-label>
+    <ion-icon :icon="closeCircle"></ion-icon>
+  </ion-chip>
 
-      <ExploreContainer name="Tab 1 page" />
+      <!-- <ExploreContainer name="Tab 1 page" /> -->
+      <div class="week-div">
+        <div class="section-header">
+          <ion-label>Week Summary</ion-label>
+        </div>
+        <Bar :data="weeklySalesData" :options="chartOptions" />
+      </div>
+
+      <div class="month-div">
+        <div class="section-header">
+          <ion-label>Months Summary</ion-label>
+        </div>
+        <Bar :data="monthlySalesData" :options="chartOptions" />
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {onMounted, ref} from 'vue'
+import { IonAvatar, IonChip, IonPage, IonHeader, IonIcon, IonLabel, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import {close, closeCircle} from 'ionicons/icons'
+import {Bar} from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+//import ExploreContainer from '@/components/ExploreContainer.vue';
+
+const daysOfTheWeek = ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+const weekDayNames = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const d = new Date();
+
+const weeklySalesData = {
+  labels: daysOfTheWeek,
+  datasets: [
+    {
+      label: "Weekly Sales Summary",
+      data: [600, 250, 150, 300, 100, 130],
+      backgroundColor: [
+        "rgba(255, 205, 86, 0.8)",
+        "rgba(75, 192, 192, 0.8)",
+        "rgba(54, 162, 235, 0.8)",
+        "rgba(153, 102, 255, 0.8)",
+        "rgba(201, 203, 207, 0.8)",
+        "rgba(75, 192, 192, 0.8)",
+      ],
+    },
+  ],
+};
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const monthlySalesData = {
+  labels: months,
+  datasets: [
+    {
+      label: "Monthly Summary",
+      data: [
+        1400, 1250, 1050, 3000, 1000, 1400, 1250, 1050, 3000, 1000, 2500, 6000,
+      ],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+        "rgba(255, 205, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(201, 203, 207, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+        "rgba(255, 205, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+      ],
+    },
+  ],
+};
+
+const chartOptions = {
+  responsive: true,
+};
 </script>
+
+<style scoped>
+.section-header {
+  display: flex;
+  flex-direction: row;
+}
+
+.week-div {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20%;
+}
+
+.month-div {
+  margin-top: 25%;
+}
+</style>
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,23 +1,141 @@
 <template>
   <ion-page>
-    <ion-header>
+    <!-- <ion-header>
       <ion-toolbar>
         <ion-title>Tab 2</ion-title>
       </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
+    </ion-header> -->
+    <ion-content :fullscreen="true" class="ion-padding">
+      <!-- <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
-      </ion-header>
+      </ion-header> -->
+      <div class="section-div">
+        <h2>Record new sale</h2>
+        <div>Lorem ipsum dolor sit, am qui enim dolorum.</div>
+        <ion-item id="radio-item">
+          <ion-label>Paid</ion-label>
+            <div id="radio-buttons-div">
+              <input
+                type="radio"
+                id="cash"
+                value="cash"
+                v-model="paymentType"
+              />
+              <label for="cash" class="radio-label">Cash</label>
+              <input
+                type="radio"
+                id="transfer"
+                value="transfer"
+                v-model="paymentType"
+              />
+              <label for="transfer" class="radio-label">Transfer</label>
+            </div>
+          </ion-item>
+          <div id="payment-total-div">
+            <p>Payment: {{ paymentType }}</p>
+            <p>Total: {{ orderTotal }}</p>
+          </div>
+          <ion-item v-if="paymentType === 'transfer'">
+            <ion-input
+              id="transfer-details"
+              placeholder="Enter details of transfer payment"
+              v-model="transferDetails"
+            ></ion-input>
+          </ion-item>
+          <div class="save-div">
+            <ion-button shape="round" :disabled="paymentType === '---'">Save</ion-button>
+          </div>
+          <ion-searchbar
+          show-clear-button="focus"
+          v-model="filterTerm"
+          placeholder="Search"
+        ></ion-searchbar>
+      </div>
+      <div class="section-div">
+        <ion-list>
+          <div class="menu-item" v-for="(p, i) in data" :key="i">
+            <ion-label>{{ p.title}}</ion-label>
+            <span>{{ p.price }}</span>
+          </div>
+        </ion-list>
+      </div>
 
-      <ExploreContainer name="Tab 2 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { computed, onMounted, ref } from "vue";
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonInput,
+  IonLabel,
+  IonList,
+  IonMenuButton,
+  IonPage,
+  IonSearchbar,
+  IonSelect,
+  IonSelectOption,
+  IonTextarea,
+  IonToolbar,
+} from "@ionic/vue";
+
+const paymentType = ref("---")
+const transferDetails = ref("")
+const orderTotal = ref(0)
+const filterTerm = ref("")
+
+const data = [
+{
+        "title": "Egg",
+        "price": "150",
+        "portion": "1"
+      },
+      {
+        "title": "Ogbono",
+        "price": "800",
+        "portion": "1"
+      },
+      {
+        "title": "Eba",
+        "price": "100",
+        "portion": "1"
+      },
+      {
+        "title": "Semovita",
+        "price": "100",
+        "portion": "1"
+      },
+      {
+        "title": "Spaghetti",
+        "price": "200",
+        "portion": "1"
+      },
+]
 </script>
+
+<style scoped>
+#payment-total-div {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  padding: 8px;
+}
+
+.radio-label {
+  padding-left: 12px;
+  padding-right: 12px;
+}
+
+#radio-item {
+  display: flex;
+  flex-direction: row;
+}
+</style>
