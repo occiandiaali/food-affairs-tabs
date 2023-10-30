c894cd3248931ece05eea09304731d6af67a9353
occiandiaali <ocean.diaali@gmail.com>
2023-10-30 15:24:01 +0100

    Add feature screens and debug create page

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -12,10 +12,43 @@
         </ion-toolbar>
       </ion-header> -->
       <div class="section-div">
-        <h2>Record new sale</h2>
-        <div>Lorem ipsum dolor sit, am qui enim dolorum.</div>
+        <!-- <h2>Record new sale</h2> -->
+        <div class="order-items-div">
+          <ion-list class="order-items-div-list" v-if="order.length">
+            <!-- <ion-item class="order-item">
+              <ion-text>Egg roll</ion-text>
+              <ion-text>2 portions</ion-text>
+              <ion-text>300 NGN</ion-text>
+            </ion-item>
+            <ion-item class="order-item">
+              <ion-text>Ogbono</ion-text>
+              <ion-text>1 portion(s)</ion-text>
+              <ion-text>800 NGN</ion-text>
+            </ion-item>
+            <ion-item class="order-item">
+              <ion-text>Egg roll</ion-text>
+              <ion-text>2 portions</ion-text>
+              <ion-text>300 NGN</ion-text>
+            </ion-item>
+            <ion-item class="order-item">
+              <ion-text>Ogbono</ion-text>
+              <ion-text>1 portion(s)</ion-text>
+              <ion-text>800 NGN</ion-text>
+            </ion-item> -->
+            <ion-item class="order-item" v-for="(item, i) in order" :key="i">
+              <ion-text>{{ item.title }}</ion-text>
+              <ion-text>{{item.portion}} portions</ion-text>
+              <ion-text>NGN {{ item.unit }}</ion-text>
+            </ion-item>
+          </ion-list>
+          <div v-else>
+            <ion-item class="order-item">
+              <ion-label>What was sold shows here...</ion-label>
+            </ion-item>
+          </div>
+        </div>
         <ion-item id="radio-item">
-          <ion-label>Paid</ion-label>
+          <!-- <ion-label>Paid</ion-label> -->
             <div id="radio-buttons-div">
               <input
                 type="radio"
@@ -35,7 +68,7 @@
           </ion-item>
           <div id="payment-total-div">
             <p>Payment: {{ paymentType }}</p>
-            <p>Total: {{ orderTotal }}</p>
+            <p>Total: {{ totalMsg }}</p>
           </div>
           <ion-item v-if="paymentType === 'transfer'">
             <ion-input
@@ -44,8 +77,20 @@
               v-model="transferDetails"
             ></ion-input>
           </ion-item>
+          <ion-item>
+            <ion-input
+              id="customer-contact"
+              label="Customer contact"
+              type="tel"
+              placeholder="888-888-8888"
+              v-model="customerPhone"
+            ></ion-input>
+          </ion-item>
           <div class="save-div">
-            <ion-button shape="round" :disabled="paymentType === '---'">Save</ion-button>
+            <ion-button shape="round" :disabled="paymentType === '--'" @click="saveOrderEntry">{{ savingEntry ? "Wait.." : "Save" }}</ion-button>
+            <div v-if="paymentType !== '--' || transferDetails.length || order.length">
+            <ion-icon @click="orderClear" :icon="trashOutline" color="danger"></ion-icon>
+          </div>
           </div>
           <ion-searchbar
           show-clear-button="focus"
@@ -53,11 +98,25 @@
           placeholder="Search"
         ></ion-searchbar>
       </div>
-      <div class="section-div">
+      <div class="menu-section-div">
         <ion-list>
-          <div class="menu-item" v-for="(p, i) in data" :key="i">
-            <ion-label>{{ p.title}}</ion-label>
-            <span>{{ p.price }}</span>
+          <div class="menu-item" v-for="(p, i) in filteredItems" :key="i">
+            <div id="item-price-col">
+              <ion-label id="item-label">{{ p.title}}</ion-label>
+            <span id="unit-cost">NGN {{ p.price }}</span>
+            </div>
+            <ion-list>
+              <ion-item>
+                <input :name="p.title" @input="portionInput($event)" style="width: 100px; margin-top: 6px;" type="number" placeholder="portion" v-model="p.portion" />
+              </ion-item>
+            </ion-list>
+            <input
+              type="checkbox"
+              aria-label="menu item"
+              :id="i.toString()"
+              :disabled="goodToGo === false"
+              @change="menuItemCheckAction($event, p.price, p.title, p.portion)"
+            />
           </div>
         </ion-list>
       </div>
@@ -83,45 +142,149 @@ import {
   IonSearchbar,
   IonSelect,
   IonSelectOption,
+  IonText,
   IonTextarea,
   IonToolbar,
 } from "@ionic/vue";
+import { trashOutline } from "ionicons/icons";
+import dummydata from './../assets/dummy-menu.json'
 
-const paymentType = ref("---")
+const paymentType = ref("--")
 const transferDetails = ref("")
+const customerPhone = ref('')
 const orderTotal = ref(0)
-const filterTerm = ref("")
+const totalMsg = computed(() => orderTotal.value)
 
-const data = [
-{
-        "title": "Egg",
-        "price": "150",
-        "portion": "1"
-      },
-      {
-        "title": "Ogbono",
-        "price": "800",
-        "portion": "1"
-      },
-      {
-        "title": "Eba",
-        "price": "100",
-        "portion": "1"
-      },
-      {
-        "title": "Semovita",
-        "price": "100",
-        "portion": "1"
-      },
-      {
-        "title": "Spaghetti",
-        "price": "200",
-        "portion": "1"
-      },
-]
+let savingEntry = false
+
+
+const filterTerm = ref("")
+const filteredItems = computed(() => {
+  return dummydata.products.filter((item) => {
+    return (
+      item.title.toLowerCase().indexOf(filterTerm.value.toLowerCase()) != -1
+    )
+  })
+})
+
+const order = ref<{
+  title: string,
+  unit: string,
+  portion: string
+}[]>([])
+
+const orderClear = () => {
+  orderTotal.value = 0
+  paymentType.value = '--'
+  transferDetails.value = ""
+  customerPhone.value = ""
+  order.value = []
+}
+
+const checkedItem = (price: string, title: string, portion: string) => {
+  const total: number = +portion * +price 
+  orderTotal.value += total;
+  order.value.push({title, unit: price, portion})
+}
+const uncheckedItem = (price: string, title: string, portion: string) => {
+  orderTotal.value -= +portion * +price
+  order.value.splice(
+    order.value.findIndex((v) => v.title === title), 1
+  )
+}
+
+const goodToGo = ref(false)
+const portionInput = (ev: any) => {
+  if (ev.target.value) {
+    goodToGo.value = true
+  } 
+  goodToGo.value = false
+}
+
+
+
+const menuItemCheckAction = (event: any, price: string, title: string, portion: string) => {
+  if (event.target.checked) {
+    checkedItem(price, title, portion)
+    console.log(`Checked ${portion} portion(s) of ${title}`)
+  } else {
+    uncheckedItem(price, title, portion)
+    console.log(`Unchecked ${portion} portion(s) of ${title}`)
+  }
+}
+
+const saveOrderEntry = async () => {
+  savingEntry = true;
+setTimeout(() => {
+  console.log(`
+  Entry: Total: ${totalMsg.value}
+  Order: ${JSON.stringify(order.value)}
+  Paid via: ${paymentType.value}
+  Customer: ${customerPhone.value}
+  `)
+  orderClear()
+  window.location.reload()
+  savingEntry = false
+}, 5000)
+}
 </script>
 
 <style scoped>
+ion-text {
+  margin-left: 4px;
+}
+
+#item-label {
+  font-size: 18px;
+  font-weight: bold;
+
+}
+
+#item-price-col {
+  display: flex;
+  flex-direction: column;
+}
+
+.menu-item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 68px;
+  border: 1px solid rgb(77, 71, 83);
+  border-radius: 6px;
+  padding: 6px;
+  margin: 4px 0px 8px 0px;
+}
+
+.menu-section-div {
+  width: 90vw;
+  max-height: 280px;
+  /* border: 1px solid rgb(221, 185, 185); */
+  margin-top: 6px;
+  padding: 4px;
+  overflow-y: auto;
+}
+.order-item {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  border: 1px solid rgb(189, 185, 185);
+  border-radius: 6px;
+  margin: 2px 0px 4px 0px;
+}
+.order-items-div {
+  width: 90vw;
+  max-height: 180px;
+  /* border: 1px solid rgb(235, 150, 150); */
+  padding: 6px;
+}
+
+.order-items-div-list {
+  max-height: 120px;
+  overflow-y: auto;
+}
+
 #payment-total-div {
   display: flex;
   flex-direction: row;
@@ -137,5 +300,17 @@ const data = [
 #radio-item {
   display: flex;
   flex-direction: row;
+}
+
+.save-div {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  margin: 6px;
+}
+
+#unit-cost {
+  color: grey;
 }
 </style>
--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -1,23 +1,108 @@
 <template>
   <ion-page>
-    <ion-header>
+    <!-- <ion-header>
       <ion-toolbar>
         <ion-title>Tab 3</ion-title>
       </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
+    </ion-header> -->
+    <ion-content :fullscreen="true" class="ion-padding">
+      <!-- <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 3</ion-title>
         </ion-toolbar>
-      </ion-header>
+      </ion-header> -->
 
-      <ExploreContainer name="Tab 3 page" />
+      <!-- <ExploreContainer name="Tab 3 page" /> -->
+      <div class="content">
+      <ion-avatar>
+    <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+    <span>@admin3000</span>
+  </ion-avatar>
+  <div id="btn-div-grid">
+    <div class="btn-div" aria-roledescription="button" @click="setOpen(true)">
+      <ion-icon :icon="menuOutline" color="warning" size="large"></ion-icon>
+      <ion-label>Menu</ion-label>
+    </div>
+    <div class="btn-div" aria-roledescription="button">
+      <ion-icon :icon="fileTrayFullOutline" color="warning" size="large"></ion-icon>
+      <ion-label>History</ion-label>
+    </div>
+    <div class="btn-div" aria-roledescription="button">
+      <ion-icon :icon="chatboxOutline" color="warning" size="large"></ion-icon>
+      <ion-label>Chats</ion-label>
+    </div>
+    <div class="btn-div" aria-roledescription="button">
+      <ion-icon :icon="settingsOutline" color="warning" size="large"></ion-icon>
+      <ion-label>Settings</ion-label>
+    </div>
+  </div>
+</div>
+<ion-modal :is-open="isOpen">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Modal</ion-title>
+          <ion-buttons slot="end">
+            <ion-button @click="setOpen(false)">Close</ion-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="ion-padding">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illum quidem recusandae ducimus quos
+          reprehenderit. Veniam, molestias quos, dolorum consequuntur nisi deserunt omnis id illo sit cum qui. Eaque,
+          dicta.
+        </p>
+      </ion-content>
+    </ion-modal>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+ import { IonAvatar, IonPage, IonButtons, IonButton, IonIcon, IonModal, IonHeader, IonLabel, IonToolbar, IonContent, IonTitle } from '@ionic/vue';
+  import { ref } from 'vue';
+  import {menuOutline, fileTrayFullOutline, settingsOutline, chatboxOutline} from 'ionicons/icons'
+//import ExploreContainer from '@/components/ExploreContainer.vue';
+
+const isOpen = ref(false);
+
+const setOpen = (open: boolean) => (isOpen.value = open);
 </script>
+
+<style scoped>
+
+.btn-div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 150px;
+  border-radius: 100%;
+  background-color: rgb(78, 42, 78);
+  cursor: pointer;
+}
+.btn-div > ion-label {
+  color: white;
+}
+
+#btn-div-grid {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px;
+  margin-top: 30%;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+ion-avatar {
+  margin-top: 26%;
+}
+
+ion-label {
+  padding-top: 6px;
+}
+</style>
